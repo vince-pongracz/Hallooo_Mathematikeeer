f69cfd9bfcfad894feec0b22d7fc98d08d21e9d5
Vince Pongracz <pongrvin@gmail.com>
2021-02-24 18:12:18 +0100

    Small example in main. Refactored some diagram.

--- ideaProject/src/main/java/org/asteroidapp/AppController.java
+++ ideaProject/src/main/java/org/asteroidapp/AppController.java
@@ -1,4 +1,9 @@
+package org.asteroidapp;
+
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 
@@ -10,11 +15,18 @@ public class AppController extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        Button button1 = new Button("Hi team! :)");
+        StackPane root = new StackPane();
+        root.getChildren().add(button1);
+        Scene scene = new Scene(root);
 
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Asteroids - beta");
+        primaryStage.show();
     }
 
     public static void main(String[] args){
         System.out.println("Hello Team! :)");
-
+        launch(args);
     }
 }
