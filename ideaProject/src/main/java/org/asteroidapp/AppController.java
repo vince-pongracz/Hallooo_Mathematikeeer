0c8f3eee0b7315ea2ac2e3e950f53b89860d73ec
Vince Pongracz <pongrvin@gmail.com>
2021-02-24 17:10:11 +0100

    JavaFX is on the board :)

---
+++ ideaProject/src/main/java/org/asteroidapp/AppController.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class AppController extends Application{
+
+    public void init(){
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+    public static void main(String[] args){
+        System.out.println("Hello Team! :)");
+
+    }
+}
